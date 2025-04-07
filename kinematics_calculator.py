58284d2b8e58d863c26b17e4223bc6f569320a76
Rohan Malhotra <104740676+rohanmalhotra0@users.noreply.github.com>
2025-04-07 09:37:17 -0400

    Added more formulas

--- kinematics_calculator.py
+++ kinematics_calculator.py
@@ -107,5 +107,62 @@ def calculate_kinematics():
         calculate_acceleration()
     else:
         print("Invalid choice.")
+
+def calculate_electric_field():
+    print("Electric Field Calculator")
+    print("Choose a setting:")
+    print("1. Lambda")
+    print("2. Radius")
+    print("3. Length")
+    setting = input()
+    
+    if setting == "l":
+        Lambda = float(input("What is the value of Lambda: "))
+        Radius = float(input("Range of values for Radius 1-"))
+        Length = float(input("What is the value of Length: "))
+        
+        xpoints = np.linspace(1, Lambda, num=int(Lambda))
+        ypoints = np.zeros_like(xpoints)
+        
+        for i in range(len(xpoints)):  
+            ypoints[i] = (1 / (4 * np.pi * sc.epsilon_0)) * (2 * xpoints[i] / Radius) / (
+                np.sqrt(1 + (4 * Radius**2) / (Length**2))
+            )
+        
+        plt.plot(xpoints, ypoints, 'o', label="Electric Field")
+        plt.xlabel("Position along length")
+        plt.ylabel("Electric Field (V/m)")
+        plt.title("Electric Field Distribution")
+        plt.legend()
+        plt.grid()
+        
+        for i in range(len(xpoints)):
+            print("Lambda Value:" + str(xpoints[i]))
+            print("E Field Value:" + str(ypoints[i]))
+        
+        plt.show()
+    elif setting == "r":
+        Lambda = float(input("What is the value of Lambda: "))
+        Radius = float(input("Range of values for Radius 1-"))
+        Length = float(input("What is the value of Length: "))
+        
+        xpoints = np.linspace(1, Radius, num=int(Radius))
+        ypoints = np.zeros_like(xpoints)
+        
+        for i in range(len(xpoints)):  
+            ypoints[i] = (1 / (4 * np.pi * sc.epsilon_0)) * (2 * xpoints[i] / Radius) / (
+                np.sqrt(1 + (4 * Radius**2) / (Length**2))
+            )
+        
+        plt.plot(xpoints, ypoints, 'o', label="Electric Field")
+        plt.xlabel("Position along length")
+        plt.ylabel("Electric Field (V/m)")
+        plt.title("Electric Field Distribution")
+        plt.legend()
+        plt.grid()
+        
+        for i in range(len(xpoints)):
+            print("Radius Value:" + str(xpoints[i]))
+
         
     
